de5054ff5c80433af3f3bb408bd2a93301dcda29
Kim Dong Geon <ehdrjs4502@naver.com>
2022-08-12 23:03:24 +0900

    220812

--- src/App.js
+++ src/App.js
@@ -11,27 +11,42 @@ const API_KEY = '4488155143302172cf93dc5fb307ec3c'
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w300/'
 const GENRES_URL = 'https://api.themoviedb.org/3/genre/movie/list?'
 
+const requests = {
+  fetchTrending: `/movie/now_playing?api_key=${API_KEY}&language=ko-KR&region=KR&page=1&primary_release_year=2022`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&language=ko-KR&region=KR&page=1&primary_release_year=2022&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=ko-KR&region=KR&page=1&primary_release_year=2022`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&language=ko-KR&region=KR&page=1&primary_release_year=2022&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&language=ko-KR&region=KR&page=1&primary_release_year=2022&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&language=ko-KR&region=KR&page=1&primary_release_year=2022&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&language=ko-KR&region=KR&page=1&primary_release_year=2022&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&language=ko-KR&region=KR&page=1&primary_release_year=2022&with_genres=99`,
+  fetchFindTest: `/find/616037?api_key=${API_KEY}&language=ko-KR&external_source=imdb_id`,
+}
+
 function App() {
+  const external_id = '616037'
   const [Movies, setMovies]= useState([]);
+  const [Find, setFind] = useState([]);
   const [Genres, setGenres] = useState([]);
   const [Loading, setLoading] = useState(true);
-  const [count, setCount] = useState(true);
   const getMovies = async() => {
     const response = await fetch(
       `${API_URL}api_key=${API_KEY}&language=ko-KR&region=KR&page=1&primary_release_year=2022`
       );
       const json = await response.json();
 
+      /*
       if(!count) {
         json.results.sort((a, b) => a.release_date > b.release_date ? -1 : 1); // 날짜 순으로 정렬
       }
+      */
 
       console.log(json.results);
 
       setMovies(json.results);
       setLoading(false);
     }
-
+    
   const getGenres = async() => {
     const response = await fetch(`${GENRES_URL}api_key=${API_KEY}`);
     const json = await response.json();
@@ -41,25 +56,36 @@ function App() {
     setGenres(json.genres);
   }
 
+  const findMovies = async() => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${external_id}?api_key=${API_KEY}&language=ko-KR&region=KR&&append_to_response=images,credits`
+      );
+      const json = await response.json();
+      console.log("찾은 영화 : ",json);
+      setFind(json.results);
+    }
+
   useEffect(() => {
     getMovies();
     getGenres();
-  },[count]) // 카운트가 바뀔떄마다 업데이트
+    findMovies();
+  },[]) // 카운트가 바뀔떄마다 업데이트
 
-  const prevBtn = () => {
-    setCount((current) => !current);
-  }
-
-  console.log(count);
   return (
     <div>
       {Loading ? <h1>영화 가져오는 중...</h1> : 
-      
       <div>
         <TopContainer/>
-        <button onClick={prevBtn}>{count ? "최신순" : "인기순"}</button>
-        <Slide Movies={Movies}/>
+        <Slide title="지금 뜨는 콘텐츠" fetchURL={requests.fetchTrending}/>
+        <Slide title="넷플릭스 오리지널" fetchURL={requests.fetchNetflixOriginals}/>
+        <Slide title="인기 순위" fetchURL={requests.fetchTopRated}/>
+        <Slide title="액션 영화" fetchURL={requests.fetchActionMovies}/>
+        <Slide title="코미디 영화" fetchURL={requests.fetchComedyMovies}/>
+        <Slide title="호러 영화" fetchURL={requests.fetchHorrorMovies}/>
+        <Slide title="로맨스 영화" fetchURL={requests.fetchRomanceMovies}/>
+        <Slide title="다큐멘터리 영화" fetchURL={requests.fetchDocumentaries}/>
         <br/><br/><br/><hr/>    
+
           {Movies. map(movie => 
           <Movie Genres={Genres}
                 key = {movie.id}
--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,51 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "../css/slick.css";
 import styles from '../css/slider.module.css'
 
+const API_URL = 'https://api.themoviedb.org/3'
+const API_KEY = '4488155143302172cf93dc5fb307ec3c'
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w300/'
 
-function Slide({Movies}) { // 메인 앱에서 Movies 가져오기
-    const settings = {
-      className: "center",
-      infinite: false, // 반복해서 보여줄지말지
-      slidesToShow: 6.07, // 화면에 보여줄 사진 개수
-      speed:600, //슬라이드 넘어가는 속도
-      swipeToSlide: true,
+function Slide({title, fetchURL}) {
+  const [movies, setMovies] = useState([]);
 
-      responsive: [ // 반응형 웹 구현 옵션
-		{  
-			breakpoint: 960, //화면 사이즈 960px일 때
-			settings: {
-				//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-				slidesToShow:3 
-			} 
-		},
-		{ 
-			breakpoint: 768, //화면 사이즈 768px일 때
-			settings: {	
-				//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-				slidesToShow:2 
-			} 
-		}
-	],
-
-      afterChange: function(index) {
-        console.log(
-          `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `${API_URL}${fetchURL}`
         );
-      }
-    };
-    return (
-      <div className={styles.slider}>
-        <h1>지금 뜨는 콘텐츠</h1>
-        <Slider {...settings}>
-          {Movies.map(movie => 
-            <li className={styles.slide_li}><a href="#"><img src={IMAGE_URL+movie.backdrop_path} alt="image" className={styles.img}/></a></li>
-            )}
-        </Slider>
-      </div>
-    );
+        const json = await response.json();
+        console.log(title,json.results);
+        setMovies(json.results);
+    }
+    
+    fetchData();
+
+  },[fetchURL])
+
+  const settings = {
+    className: "center",
+    infinite: false, // 반복해서 보여줄지말지
+    slidesToShow: 6.07, // 화면에 보여줄 사진 개수
+    speed:600, //슬라이드 넘어가는 속도
+    swipeToSlide: true,
+
+    responsive: [ // 반응형 웹 구현 옵션
+  {  
+    breakpoint: 960, //화면 사이즈 960px일 때
+    settings: {
+      //위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+      slidesToShow:3 
+    } 
+  },
+  { 
+    breakpoint: 768, //화면 사이즈 768px일 때
+    settings: {	
+      //위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+      slidesToShow:2 
+    } 
+  }
+],
+
+    afterChange: function(index) {
+      console.log(
+        `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
+      );
+    }
+  };
+
+
+  return (
+    <div className={styles.slider}>
+      <h1>{title}</h1>
+      <Slider {...settings}>
+        {movies.map(movie => 
+          <li className={styles.slide_li}><a href="#"><img src={IMAGE_URL+movie.backdrop_path} alt="image" className={styles.img}/></a>
+            <span className={styles.movie_title}>{movie.title}{movie.name}</span>
+          </li>
+          )}
+      </Slider>
+    </div>
+  );
 }
 
 export default Slide;
